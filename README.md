4d0b600b9e3a3232ca45ac40272f0c4fce46ad05
nedloz <andrukuzminkuzmin@yandex.ru>
2023-05-14 23:03:20 +0300

    лимитер перенесен в отдельный файл и импортирован как модуль в app.js. написан README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # movies-explorer-api
+
+## Запуск проекта
+
+`npm run start` — запускает сервер   
+`npm run dev` — запускает сервер с hot-reload
+
+## Ссылка
+https://github.com/nedloz/movies-explorer-api
+
+
+## Ссылки на проект
+
+IP-адрес 158.160.57.22
+
+Backend https://nedloz.diplom.nomoredomains.monster/api
--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 
 require('dotenv').config();
 
 const appRoutes = require('./routes/app');
 const centralErrorHandler = require('./middlewares/centralErrorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const limiter = require('./middlewares/limiter');
 
 const app = express();
 
@@ -18,13 +18,6 @@ try {
 } catch (err) {
   process.exit();
 }
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
 
 app.use(helmet());
 app.use(limiter);
